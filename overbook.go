1692a1c670a84eb46c6f51a51964fb6b626d9b3c
ayushn2 <iayush.n2@gmail.com>
2025-10-18 20:00:55 +0530

    feat: enable delete order

--- overbook.go
+++ overbook.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Order struct {
 	Size float64
@@ -9,6 +12,10 @@ type Order struct {
 	Timestamp int64
 }
 
+func (o *Order) String() string {
+	return fmt.Sprintf("[size: %.2f]",o.Size)
+}
+//  Limit represents a price level in the order book with associated orders.
 type Limit struct {
 	Price    float64
 	Orders []*Order
@@ -35,6 +42,19 @@ func (l *Limit) AddOrder(o *Order) {
 	o.Limit = l
 	l.Orders = append(l.Orders, o)
 	l.TotalVolume += o.Size
+}
+
+func (l *Limit) DeleteOrder(o *Order) {
+	for i := 0; i< len(l.Orders); i++ {
+		if l.Orders[i] == o {
+			l.Orders[i] = l.Orders[len(l.Orders)-1] // Move the last order to the position of the order to be deleted
+			l.Orders = l.Orders[:len(l.Orders)-1] // Remove the last order
+		}
+	}
+	o.Limit = nil
+	l.TotalVolume -= o.Size
+
+	// TODO(@ayushn2): resort orders by timestamp to maintain FIFO order
 }
 
 type Overbook struct {
--- overbook_test.go
+++ overbook_test.go
@@ -3,17 +3,33 @@ package main
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLimit(t *testing.T){
 	l := NewLimit(10000) 
-	buyOrder := NewOrder(true, 5) // struct Order { Size: 5, Bid: true, Limit: *Limit, Timestamp: int64 }
+	buyOrderA := NewOrder(true, 5) // struct Order { Size: 5, Bid: true, Limit: *Limit, Timestamp: int64 }
 
-	l.AddOrder(buyOrder)
+	buyOrderB := NewOrder(true, 10)
+	buyOrderC := NewOrder(true, 15)
+
+	
+
+	l.AddOrder(buyOrderA)
+	l.AddOrder(buyOrderC)
+	l.AddOrder(buyOrderB)
+
+	l.DeleteOrder(buyOrderB)
+	assert.Equal(t, 20.0, l.TotalVolume)
+	assert.Equal(t, 2, len(l.Orders))
+	assert.Nil(t, buyOrderB.Limit)
 
 	fmt.Println(l)
 	
 }
+
+
 
 func TestOrderBook(t *testing.T){
 	
